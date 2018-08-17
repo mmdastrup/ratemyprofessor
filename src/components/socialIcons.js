ceb1c3d78fea46ebc49b3e0ff2b305438985ad45
Peter M Souza Jr <globar10@Peters-MacBook-Pro-2.local>
2018-08-17 09:04:14 -0700

    created social icons file

---
+++ src/components/socialIcons.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SocialIcons extends Component {
+    render() {
+        return (
+            <div>
+                <span></span><span></span><span></span>
+            </div>
+        );
+    }
+}
+
+export default SocialIcons;
