1e19a5d17ad27bd5b673bdd76598c880bdee49f6
Michael Dastrup <michaelmaxdastrup@gmail.com>
2018-08-17 09:49:53 -0600

    functional login and Create Account that save data

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,6 +3,10 @@ import { Link } from "react-router-dom";
 
 
 class Navbar extends Component {
+  constructor(props) {
+    super(props)
+
+  }
   render() {
     return (
       <div className="navbar">
